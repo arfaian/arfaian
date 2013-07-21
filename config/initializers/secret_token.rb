cee0c80da083914cd2e2dda0db627d81e9652293
Arian Arfaian <arian.arfaian@gmail.com>
2013-07-21 18:17:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arfaian::Application.config.secret_key_base = 'b76321488a8cc1fb83003538c03cea692ec372812d5c5377ad2d00b7244ac1c2e7ec94a48ef08f589db3ba5abefb432601a3d7a5237ae7c283f37ca9260909f6'
