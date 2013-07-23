69af0c6a907dc9ed051ed7a62b0063b94a44fb19
Arian Arfaian <arian.arfaian@gmail.com>
2013-07-22 22:36:37 -0400

    change asset_host to cloudfront url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Arfaian::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "http://arfaian.s3.amazonaws.com"
+  config.action_controller.asset_host = "d3tooegql1lw1f.cloudfront.net"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
